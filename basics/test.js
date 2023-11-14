3c0e7d43947262414fe41fc6f81440686a171f9c
Nursid Ansari <114871863+Nursid@users.noreply.github.com>
2023-11-14 09:14:29 +0000

    container div save

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,5 @@
-console.log("Hello Nursid Ansari")
+
+var x=parseInt(prompt("Enter First Number"))
+var y=parseInt(prompt("Enter Secornd Number"))
+var sum=x+y
+document.getElementById("result").innerHTML=sum
